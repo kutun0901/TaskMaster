356812c4ff8da27f5776012111350f39f2642248
kutun0901 <tuan.tq2433@gmail.com>
2024-01-18 09:25:46 -0800

    description input styles

--- src/components/InputComponent.tsx
+++ src/components/InputComponent.tsx
@@ -14,36 +14,41 @@ interface Props {
     title?: string,
     prefix?: ReactNode,
     affix?: ReactNode,
-    allowClear?: boolean
+    allowClear?: boolean,
+    multiple?: boolean,
+    numberOfLine?: number
 }
 
 const InputComponent = (props: Props) => {
-    const { value, onChange, placeHolder, title, prefix, affix, allowClear } = props;
+    const { value, onChange, placeHolder, title, prefix, affix, allowClear, multiple, numberOfLine } = props;
     return (
         <View style={{ marginBottom: 16 }}>
             {title && <TitleComponent text={title} />}
             <RowComponent styles={[globalStyles.inputContainer,
             {
                 marginTop: title ? 8 : 0,
-                minHeight: 32,
+                minHeight: multiple && numberOfLine ? 32 * numberOfLine : 32,
                 paddingVertical: 14,
-                paddingHorizontal: 10
+                paddingHorizontal: 10,
+                alignItems: 'flex-start'
             }]}>
                 {prefix && prefix}
                 <View style={{ flex: 1 }}>
-                    <TextInput style={[globalStyles.text, { margin: 0, padding: 0, paddingVertical: 6, flex: 1 }]}
+                    <TextInput style={[globalStyles.text, { margin: 0, padding: 0, paddingVertical: 6, flex: 1,}]}
                         placeholder={placeHolder ?? ""}
                         placeholderTextColor={'#676767'}
                         value={value}
                         onChangeText={val => onChange(val)}
+                        multiline={multiple}
+                        numberOfLines={numberOfLine}
                     />
 
                 </View>
                 {affix && affix}
 
                 {allowClear && value && (
-                    <TouchableOpacity onPress={()=> onChange('')}>
-                        <AntDesign name='close' size={20} color={colors.white}/>
+                    <TouchableOpacity onPress={() => onChange('')}>
+                        <AntDesign name='close' size={20} color={colors.white} />
                     </TouchableOpacity>
                 )}
             </RowComponent>
--- src/tasks/AddNewTask.tsx
+++ src/tasks/AddNewTask.tsx
@@ -1,4 +1,4 @@
-import { View, Text } from 'react-native'
+import { View, Text, Button } from 'react-native'
 import React, { useState } from 'react'
 import Container from '../components/Container'
 import TextComponent from '../components/TextComponent'
@@ -17,27 +17,44 @@ const initValues: TaskModel = {
 }
 
 const AddNewTask = ({ navigation }: any) => {
+
     const [taskDetail, setTaskDetail] = useState<TaskModel>(initValues)
 
     const handleChangeValue = (id: string, value: string) => {
-        const item: any = {...taskDetail};
+        const item: any = { ...taskDetail };
 
         item[`${id}`] = value;
 
         setTaskDetail(item);
     }
 
+    const handleAddNewTask = async () => {
+        console.log(taskDetail)
+    }
+
     return (
         <Container back title='Add new task'>
             <SectionComponent>
                 <InputComponent
-                value={taskDetail.title}
-                onChange={val => handleChangeValue('title', val)}
-                title='Title'
-                allowClear
-                placeHolder='Task title'
+                    value={taskDetail.title}
+                    onChange={val => handleChangeValue('title', val)}
+                    title='Title'
+                    allowClear
+                    placeHolder='Task title'
                 />
 
+                <InputComponent
+                    value={taskDetail.description}
+                    onChange={val => handleChangeValue('description', val)}
+                    title='Description'
+                    allowClear
+                    placeHolder='content'
+                    multiple
+                    numberOfLine={3}
+                />
+            </SectionComponent>
+            <SectionComponent>
+                <Button title='Save' onPress={handleAddNewTask} />
             </SectionComponent>
         </Container>
     )
