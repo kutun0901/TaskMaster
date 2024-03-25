f7133d9d42d6ecdd5f21860f9d5bc04ad41094ea
kutun0901 <tuan.tq2433@gmail.com>
2024-03-25 15:37:07 -0700

    done handle fcmtoken for user

--- src/utils/handleNotification.ts
+++ src/utils/handleNotification.ts
@@ -1,7 +1,7 @@
 import auth from '@react-native-firebase/auth'
 import firestore from '@react-native-firebase/firestore'
 import messaging from '@react-native-firebase/messaging'
-
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
 const user = auth().currentUser;
 
@@ -17,6 +17,34 @@ export class HandleNotification{
     }
 
     static getFcmToken = async() => {
-        const fcmToken = await messaging().getToken()
+        const fcmtoken = await AsyncStorage.getItem('fcmtoken')
+
+        if (!fcmtoken){
+            // get fcmtoken
+
+            const token = await messaging().getToken();
+
+            if (token){
+                await AsyncStorage.setItem('fcmtoken', token);
+                this.updateToken(token);
+            }
+        }
+    }
+
+    static updateToken = async (token: string) => {
+        await firestore().doc(`users/${user?.uid}`).get().then((snap) => {
+            if (snap.exists){
+                const data: any = snap.data();
+
+                if (!data.tokens || !data.tokens.includes(token)){
+                    firestore().doc(`users/${user?.uid}`).update({
+
+                        // add token to tokens array using FieldValue from firestore
+
+                        tokens: firestore.FieldValue.arrayUnion(token)
+                    })
+                }
+            }
+        })
     }
 }
