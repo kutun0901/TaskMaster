c391ba30df27056a799fbf208e1780f64bc3e986
kutun0901 <tuan.tq2433@gmail.com>
2024-03-27 10:26:44 -0700

    notifications screen

--- src/screens/Notifications.tsx
+++ src/screens/Notifications.tsx
@@ -1,12 +1,88 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, TouchableOpacity, FlatList } from 'react-native'
+import React, { useState } from 'react'
+import { NotificationModel } from '../models/NotificationModel'
+import auth from '@react-native-firebase/auth'
+import firestore from '@react-native-firebase/firestore'
+import Container from '../components/Container'
+import TextComponent from '../components/TextComponent'
+import { fontFamilies } from '../constants/fontFamilies'
+import { colors } from '../constants/colors'
+import { HandleDateTime } from '../utils/handleDateTime'
 
-const Notifications = () => {
+const Notifications = ({navigation}: any) => {
+  const [notifications, setNotifications] = useState<NotificationModel[]>([])
+
+  const user = auth().currentUser;
+
+  const getNotifications = () => {
+    const filter = firestore().collection('notifications')
+    .where('uid', '==', user?.uid);
+
+    filter.onSnapshot(snap => {
+      if (!snap.empty){
+        const items: NotificationModel[] = [];
+
+        snap.forEach((item: any) => {
+          items.push({
+            id: item.id,
+            ...item.data()
+          })
+        })
+        setNotifications(items.sort((a: any, b: any) => a.isRead - b.isRead));
+      }
+    })
+  }
+
+
+const handleReadNotification = async ({
+    id,
+    isRead,
+    taskId,
+  }: {
+    id: string;
+    isRead: boolean;
+    taskId: string;
+  }) => {
+    try {
+      navigation.navigate('TaskDetail', {id: taskId});
+
+      !isRead &&
+        (await firestore().doc(`notifications/${id}`).update({isRead: true}));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <View>
-      <Text>Notification</Text>
-    </View>
-  )
-}
+    <Container back title="Notifications">
+      <FlatList
+        showsVerticalScrollIndicator={false}
+        data={notifications}
+        renderItem={({item}) => (
+          <TouchableOpacity
+            onPress={() =>
+              handleReadNotification({
+                id: item.id,
+                taskId: item.taskId,
+                isRead: item.isRead,
+              })
+            }
+            style={{paddingHorizontal: 16, paddingBottom: 16}}>
+            <TextComponent
+              text={item.title}
+              color={item.isRead ? colors.gray2 : colors.text}
+              font={item.isRead ? fontFamilies.regular : fontFamilies.bold}
+            />
+            <TextComponent text={item.body} size={12} />
+            <TextComponent
+              text={HandleDateTime.DateString(new Date(item.createdAt))}
+              size={12}
+            />
+          </TouchableOpacity>
+        )}
+      />
+    </Container>
+  );
+};
 
-export default Notifications
+export default Notifications;
